6c5c01f08ab78d91840bd0e7d7b2d0db7792cb11
muembiahub <jonathanmuembia3@gmail.com>
2025-04-10 07:55:53 +0200

    commit last

--- scripts/form.js
+++ scripts/form.js
@@ -1,19 +1,3 @@
-const stars = document.querySelectorAll('.rating span');  
-stars.forEach(star => {  
-  star.addEventListener('click', () => {  
-    const rating = star.getAttribute('data-value');  
-    // Appel d'une fonction pour envoyer la note au backend  
-    submitRating(rating);  
-  });  
-});  
-
-function submitRating(rating) {  
-  console.log(`Note soumise : ${rating}`);  
-  // Vous pouvez utiliser fetch ou axios pour envoyer la note à votre serveur  
-}  
-
-
-
 const currentyear = new Date().getFullYear();
 const lastModified = new Date(document.lastModified);
 document.getElementById("currentyear").textContent = currentyear;
